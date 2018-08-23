ca97645f8e56c9bb82f860bea928bf335c146d3e
Karis Kim <ouniiq@hotmail.com>
2018-08-23 14:25:44 -0700

    fixed error, all instance variables are created, passed 1st test

---
+++ lib/order.rb
@@ -0,0 +1,13 @@
+require_relative 'customer'
+
+class Order
+  attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id = id.to_i
+    @products = products
+    @customer = customer
+    @fulfillment_status = fulfillment_status
+  end
+end
