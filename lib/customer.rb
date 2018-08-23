23e44262b6944a61fa99fd09b53d41ff21578b83
Trang <trang.frego@gmail.com>
2018-08-23 13:34:47 -0700

    create Class Customer and set initialize methods

---
+++ lib/customer.rb
@@ -0,0 +1,16 @@
+class Customer
+  attr_reader :ID
+  attr_accessor :email, :delivery_address
+
+  def initialize(ID, email, delivery_address)
+    @ID = ID
+    @email = email
+    @dlivery_address = delivery_address
+  end
+
+
+
+
+
+
+end
