e4cde25f23979fc38a31f6b5b20459084ee332b7
Daniela Di Iorio <danielad@gmail.com>
2018-08-23 13:31:32 -0700

    order.rb can initialize a new order, total up an order, and add a product.

---
+++ lib/order.rb
@@ -0,0 +1,33 @@
+
+class Order
+
+  attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id = id
+    @products = products
+    @customer = customer
+
+    valid_statuses = [:pending, :paid, :processing, :shipped, :complete]
+    raise ArgumentError unless valid_statuses.include? fulfillment_status
+    @fulfillment_status = fulfillment_status
+  end
+
+  def total
+    total = 0.00
+    @products.each do |item, cost|
+      total += cost
+    end
+
+    total *= 1.075
+    return total.round(2)
+  end
+
+  def add_product(product_name, price)
+    raise ArgumentError if @products.keys.include? product_name
+    @products[product_name] = price
+  end
+
+
+end
