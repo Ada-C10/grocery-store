e5e2912f24993e6016918a1fb63430ba9803d15c
Trang <trang.frego@gmail.com>
2018-08-23 13:39:14 -0700

    create Order class and setup initialize methods

---
+++ lib/order.rb
@@ -0,0 +1,15 @@
+class Order
+  attr_reader :ID, :customer
+  attr_accessor :product_data, :fulfillment_status
+
+  def initialize(ID, product data, customer, fulfillment_status)
+    @ID = ID
+    @product_data = product_data
+    @customer = cust
+    @fulfillment_status = fulfillment_status
+  end
+
+
+
+  
+end
