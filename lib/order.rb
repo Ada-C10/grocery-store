f149a9a95de09207b37b0daff1c01db0641ae3f3
Anibel America <ameriabel@gmail.com>
2018-08-23 13:25:22 -0700

    implements total method

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+TAX = 0.075
+
 class Order
 
   attr_reader :id
@@ -16,4 +18,24 @@ class Order
     end
   end
 
+  def total
+    subtotal = @products.sum do |key, value|
+      value
+    end
+
+    total = subtotal + (subtotal * TAX)
+
+    return total.round(2)
+
+
+  end
+
 end
+
+
+# A total method which will calculate the total cost of the order by:
+# Summing up the products
+# Adding a 7.5% tax
+# Rounding the result to two decimal places
+# An add_product method which will take in two parameters, product name and price, and add the data to the product collection
+# If a product with the same name has already been added to the order, an ArgumentError should be raised
