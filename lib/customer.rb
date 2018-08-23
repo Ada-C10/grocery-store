021b2aa0e5139c3704c9ee6f09aa4dc4702ddd07
Karis Kim <ouniiq@hotmail.com>
2018-08-23 12:25:56 -0700

    created class named customer with 3 attributes

---
+++ lib/customer.rb
@@ -0,0 +1,10 @@
+class Customer
+  attr_reader :id,
+  attr_accessor :email, :address
+
+  def initialize(id, email, address)
+    @id = id.to_i
+    @email = email.to_s
+    @address = {}
+  end
+end
