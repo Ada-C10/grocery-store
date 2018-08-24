ce027f589c6776368980eb8d4af3139b909dd3ad
Daniela Di Iorio <danielad@gmail.com>
2018-08-23 21:16:43 -0700

    tests for order.all returns an array and returns accurate info about last order

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -154,7 +154,13 @@ end
 describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
-      # TODO: Your test code here!
+
+      expect(Order.all.class).must_equal Array
+
+      expect(Order.all.length).must_equal 100
+
+      expect(Order.all.all? {|item| item.class == Order})
+
     end
 
     it "Returns accurate information about the first order" do
@@ -178,7 +184,23 @@ describe "Order Wave 2" do
     end
 
     it "Returns accurate information about the last order" do
-      # TODO: Your test code here!
+      id = 100
+      products = {
+        "Amaranth" => 83.81,
+        "Smoked Trout" => 70.6,
+        "Cheddar" => 5.63
+      }
+      customer_id = 20
+      fulfillment_status = :pending
+
+      order = Order.all.last
+
+      # Check that all data was loaded as expected
+      expect(order.id).must_equal id
+      expect(order.products).must_equal products
+      expect(order.customer).must_be_kind_of Customer
+      expect(order.customer.id).must_equal customer_id
+      expect(order.fulfillment_status).must_equal fulfillment_status
     end
   end
 
