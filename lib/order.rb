f56c74c9fbec7a95165afdd1c2dfbd9948a74bc2
Katricia Smith <smithkatricia@gmail.com>
2018-08-23 19:02:44 -0700

    Added Order#total and Order#add_product to class Order

--- lib/order.rb
+++ lib/order.rb
@@ -7,13 +7,25 @@ class Order
     @products = products # hash of products and costs
     @fulfillment_status = fulfillment_status
 
-    fulfillment_options = [ :pending, :paid, :processing, :shipped :complete]
+    @fulfillment_options = [:pending, :paid, :processing, :shipped :complete]
 
     unless fulfillment_options.include? fulfillment_status
       raise ArgumentError, 'Fulfillment status must be :pending, :paid, :processing, :shipped, or :complete. '
     end # end of Argument Error unless
-
   end # end of def initialize
 
+
+  def total(product_sum)
+    @product_sum = products[:cost].sum * .075
+  end # end of def total
+
+
+  def add_product(product_name, product_price)
+    if @products.include? product.name
+      raise ArgumentError, "Product is already on product list."
+    end
+
+    @products << {product_name: product_price}
+  end # end of def add_product
 
 end # end of class Order
