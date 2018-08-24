76c3e55d03833c5d86a3a99124ab9871f29f3072
Jane <jane.edwards.mcnear@gmail.com>
2018-08-23 22:04:47 -0700

    Completed Wave 1

---
+++ lib/order.rb
@@ -0,0 +1,36 @@
+require_relative 'customer'
+require 'pry'
+
+class Order
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id = id
+    @products = products
+    @customer = customer
+
+    if fulfillment_status != :pending && fulfillment_status != :paid && fulfillment_status != :processing  && fulfillment_status != :shipped && fulfillment_status != :complete
+      raise ArgumentError, 'Status must be: :pending, :paid, :processing, :shipped, :complete'
+    else
+      @fulfillment_status = fulfillment_status
+      @fulfillment_status ||= :pending
+    end
+
+  end
+
+  attr_reader(:id, :products, :fulfillment_status, :customer, :total)
+
+  def total
+    sum = products.values.sum
+    percent = sum * 0.075
+    return @total = ("%.2f" % (sum + percent)).to_f
+  end
+
+  def add_product(name, price)
+    if @products.keys.include?(name)
+      raise ArgumentError, 'A product with the same name has already been added to the order'
+    else
+      return products[name] =  price
+    end
+  end
+
+end
