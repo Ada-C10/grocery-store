4008264cfc11b60bf68822d1494ffccdf05fb95e
Daniela Di Iorio <danielad@gmail.com>
2018-08-24 15:47:46 -0700

    refactor customer.rb.  no need for initialize to add customer to customer_list.  no need for customer_list to be a class variable

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,31 +1,21 @@
-require 'pry'
 require 'csv'
-
 
 class Customer
 
   attr_reader :id
   attr_accessor :email, :address
 
-  @@customer_list = []
-
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
-    @@customer_list << self
-  end
-
-  def self.fill_customer_list
-    @@customer_list = CSV.open("data/customers.csv", "r").map do |line|
-       self.new(line[0].to_i, line[1], {address1: line[2], city: line[3], state: line[4], zip_code: line[5]})
-    end
-
   end
 
   def self.all
-    self.fill_customer_list
-    return @@customer_list
+    customer_list = CSV.open("data/customers.csv", "r").map do |line|
+       self.new(line[0].to_i, line[1], {street: line[2], city: line[3], state: line[4], zip_code: line[5]})
+    end
+    return customer_list
   end
 
   def self.find(id)
