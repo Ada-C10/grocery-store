1009320a0a3e4c9c24f4a4afe2ff8c8ed82fb411
Amber Lynn <griffiam@gmail.com>
2018-08-23 22:51:03 -0700

    cancelled out tests that I wont be working on. Added if conditionals - must use exceptions better

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,23 @@
 class Customer
   attr_reader :id
   attr_accessor :email, :address
+
   def initialize(ID, EMAIL, ADDRESS)
     @ID = ID
+    if @ID.class != integer
+      puts "Not a num"
+    end
+
     @EMAIL = EMAIL
+    if @EMAIL.class != string
+      puts "not a string"
+    end
+
     @ADDRESS = ADDRESS
+    if @ADDRESS.class != hash
+      puts "not a hash of address data"
+    end
   end
+
+
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,23 @@
 class Order
   attr_reader :id
-  def initialize(id, product_cost, customer, status)
+
+  def initialize(id, product_cost, customer, status=':pending')
     @id = id
+    if @id.class != integer
+      puts "not a num"
+    end
+
     @product_cost = product_cost
+    if @product_cost.class != hash
+      puts "not a hash"
+    end
+
     @customer = customer
+
     @status = status
+    if @status.class != string
+      puts "not a string"
+    end
   end
 
   def total()
