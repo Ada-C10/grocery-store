ddd803c504b7e34fd39a2abab78093d2e86b5f5d
jackie <jacquelyn.cheng@me.com>
2018-08-24 13:31:18 -0700

    First attempt at Wave 2 Customer section

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,5 +8,23 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  # returns an array of Customer objects
+  def self.all
+    all_customers = CSV.open("data/customers.csv").map do |row|
+      id = row[0].to_i
+      email = row[1]
+      address = {street: row[2], city: row[3],
+        state: row[4], zip: row[5]}
+      Customer.new(id, email, address)
+    end
+    return all_customers
+  end
+
+  # returns a Customer object matching the designated id
+  def self.find(id)
+    all_customers = Customer.all
+    return all_customers[id-1]
   end
 end
