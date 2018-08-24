00168a61fbf5cbca63cb6201af389b6e5ef42941
jackie <jacquelyn.cheng@me.com>
2018-08-24 14:24:18 -0700

    Wrote tests for Wave 2 Orders section

--- lib/order.rb
+++ lib/order.rb
@@ -9,6 +9,29 @@ class Order
     @fulfillment_status = fulfillment_status
   end
 
+  # returns an array of all orders
+  def self.all
+    all_orders = CSV.open("data/orders.csv").map do |row|
+      id = row[0].to_i
+      product_list = row[1].split(';')
+      products = {}
+      product_list.each do |product_data|
+        product_and_price = product_data.split(':')
+        products[product_and_price[0]] = product_and_price[1]
+      end
+      customer = Customer.find(row[2].to_i)
+      fulfillment_status = row[3].to_sym
+      Order.new(id, products, customer, fulfillment_status)
+    end
+    return all_orders
+  end
+
+  # returns an Order instance matching the designated id
+  def self.find(id)
+    all_orders = Order.all
+    return all_orders[id-1]
+  end
+
   def total
     return (@products.values.sum * 1.075).round(2)
     # A `total` method which will calculate the total cost of the order by:
@@ -26,8 +49,6 @@ class Order
     # An `add_product` method which will take in two parameters, product name and price, and add the data to the product collection
     #   - If a product with the same name has already been added to the order, an `ArgumentError` should be raised
   end
-
-
 
 
 end
