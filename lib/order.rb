bff96da8dfbe7d57f9864f97a619952a119cdf4b
Lindsay Terchin <lterchin@gmail.com>
2018-08-23 19:50:08 -0700

    added method to validate fulfillment status

--- lib/order.rb
+++ lib/order.rb
@@ -5,17 +5,19 @@ class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, products, customer, fulfillment_status)
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
+        validate_fulfillment_status
   end
 
-
-
-
-
-
+  def validate_fulfillment_status
+    fulfillment_status_options = [:pending, :paid, :processing, :shipped, :complete]
+    if fulfillment_status_options.include?(fulfillment_status) == false
+      raise ArgumentError.new("Fulfillment status :pending, :paid, :processing, :shipped, or :complete")
+    end
+  end
 
 end
