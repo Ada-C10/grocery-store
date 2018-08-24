4331f3bfe00e7b74e8ea4dbc2936eebc7f16ef3b
Lindsay Terchin <lterchin@gmail.com>
2018-08-23 19:02:24 -0700

    created Order class that passes initialization test

---
+++ lib/order.rb
@@ -0,0 +1,21 @@
+require_relative 'customer'
+
+class Order
+
+  attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
+
+  def initialize(id, products, customer, fulfillment_status)
+    @id = id
+    @products = products
+    @customer = customer
+    @fulfillment_status = fulfillment_status
+  end
+
+
+
+
+
+
+
+end
