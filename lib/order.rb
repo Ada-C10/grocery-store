6ab929c9c1538b56a31e436e135cb7fc58a234c7
Amanda Ungco <amanda.ungco@gmail.com>
2018-08-23 23:16:36 -0700

    created Order initalize method

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,12 @@
 class Order
   def initialize(id, products, customer, fulfillment_status = :pending)
-    @id
-    @products = {}
+    @id = id
+    @products = products #hash {item: cost}
     @customer = customer
-    @fulfillment_status ||= :pending
+    raise ArgumentError, "Invalid fulfillment status" while fulfillment_status != nil,:pending, :paid, :processing, :shipped, :complete
+    @fulfillment_status = fulfillment_status
   end
 
-  attr_reader :id,
+  attr_reader :id
   attr_accessor :products, :products, :customer, :fulfillment_status
 end
