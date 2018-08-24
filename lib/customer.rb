d93b95a205bee16c8e72c0e39a0f1519015e3d58
Amanda Ungco <amanda.ungco@gmail.com>
2018-08-24 16:01:48 -0700

    added self.all method and @@customers =[]

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,12 +1,16 @@
 require 'csv'
 class Customer
   attr_reader :id, :email, :address
-  attr_accessor :email, :address
+  attr_accessor :email, :address, :customers
+
+  @@customers = []
 
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
+
+    @@customers << self
   end
 
   def self.load_data(filename) #take file name and returns data from file in array of hashes
@@ -17,7 +21,6 @@ class Customer
   end
 
   def self.format_data(data)
-      customers = []
       data.each do |individual|
         id = individual[0]
         email = individual[1]
@@ -25,20 +28,25 @@ class Customer
                    city: individual[3],
                    state: individual[4],
                    zip: individual[5]}
-        customers << self.new(id, email, address)
+        self.new(id, email, address)
       end
-    return customers
+    return @@customers
   end
 
    def self.all
-     return #collection of Customer instances - all of the ifno from csv file
+     return @@customers#collection of Customer instances - all of the ifno from csv file
    end
 
    def self.find(id)
+     @@customers.each do |@id|
+       if id == @id
+         return self
+
      #search thgough the customer.all not the csv
      return #instance of customer where value of id in csv matches th parameter`
    end
 end
 
 
-p Customer.format_data(Customer.load_data('../data/test.csv'))
+Customer.format_data(Customer.load_data('../data/test.csv'))
+p Customer.all
