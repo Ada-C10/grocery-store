1cc58424ee5a3928116363511dc201d6313965a0
Katricia Smith <smithkatricia@gmail.com>
2018-08-23 21:48:19 -0700

    Passed fulfillment status pending test

--- lib/order.rb
+++ lib/order.rb
@@ -2,30 +2,43 @@ class Order
 
   attr_reader :id
 
-  def initialize(id, products, Customer, fulfillment_status: pending)
+  attr_accessor :products, :customer, :fulfillment_status
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products # hash of products and costs
+
+    puts fulfillment_status
     @fulfillment_status = fulfillment_status
 
-    @fulfillment_options = [:pending, :paid, :processing, :shipped :complete]
+    @fulfillment_options = [:pending, :paid, :processing, :shipped, :complete]
 
-    unless fulfillment_options.include? fulfillment_status
+    unless @fulfillment_options.include? @fulfillment_status
       raise ArgumentError, 'Fulfillment status must be :pending, :paid, :processing, :shipped, or :complete. '
     end # end of Argument Error unless
   end # end of def initialize
 
 
-  def total(product_sum)
-    @product_sum = products[:cost].sum * .075
+  def total(products)
+    # @product_sum = products[:cost].sum * 1.075
+    if @products.empty?
+      @product_sum = 0
+    else
+      @product_sum = @products.values.sum
+    end
+
+    return @product_sum
   end # end of def total
 
 
   def add_product(product_name, product_price)
-    if @products.include? product.name
+    if @products.include? product_name
       raise ArgumentError, "Product is already on product list."
+    else
+      @products << {product_name: product_price}
     end
 
-    @products << {product_name: product_price}
+
   end # end of def add_product
 
-end # end of class Order
+end # end of class Order # why unexpected??
