f798725b3c6942b8bc350d67335af1c70a2f114a
Alice Hsiao <alice.hsiao87@gmail.com>
2018-08-24 07:47:31 -0700

    set up class Customer and Order with functionality as described in Wave 1 (all tests passed)

---
+++ lib/order.rb
@@ -0,0 +1,44 @@
+require 'pry'
+require 'awesome_print'
+
+class Order
+  attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id = id
+    @products = products
+    @customer = customer
+    if [:pending, :paid, :processing, :shipped, :complete].include? fulfillment_status
+      @fulfillment_status = fulfillment_status
+    else
+      raise ArgumentError.new("This is not a valid fulfillment status.")
+    end
+  end
+
+  def total
+    if @products.length == 0
+      return 0
+    else
+      total_cost = @products.values.reduce(:+)
+      total_cost *= 1.075
+      return total_cost.round(2)
+    end
+  end
+
+  def add_product(name, price)
+    if @products[name] == nil
+      @products[name] = price
+    else
+      raise ArgumentError.new("This product has already been added.")
+    end
+  end
+
+  def remove_product (name)
+    if @products[name] == nil
+      raise ArgumentError.new("This product cannot be found.")
+    else
+      @products.delete(name)
+    end
+  end
+end
--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -111,10 +111,40 @@ describe "Order Wave 1" do
       expect(order.total).must_equal before_total
     end
   end
+
+  describe "#remove_product" do
+    it "Decreases the number of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00, "salad" => 4.25}
+      before_count = products.count
+      order = Order.new(1337, products, customer)
+
+      order.remove_product("banana")
+      expected_count = before_count - 1
+      expect(order.products.count).must_equal expected_count
+    end
+
+    it "Is removed to the collection of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00, "sandwich" => 4.25 }
+      order = Order.new(1337, products, customer)
+
+      order.remove_product("cracker")
+      expect(order.products.include?("cracker")).must_equal false
+    end
+
+    it "Raises an ArgumentError if the product is not present in remove_product call" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+
+      order = Order.new(1337, products, customer)
+
+      expect {
+        order.remove_product("salad", 5.25)
+      }.must_raise ArgumentError
+    end
+  end
 end
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
-describe "Order Wave 2" do
+xdescribe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
       # TODO: Your test code here!
@@ -145,7 +175,7 @@ describe "Order Wave 2" do
     end
   end
 
-  describe "Order.find" do
+  xdescribe "Order.find" do
     it "Can find the first order from the CSV" do
       # TODO: Your test code here!
     end
