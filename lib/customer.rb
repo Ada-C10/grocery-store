08664a97842fed7c5a41f7777bed6a4dcdbfb959
Laura <larmartinez2@gmail.com>
2018-08-24 15:14:06 -0700

    creates an instances of customer and saves it in an array, this is created using self.all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,31 @@
+require 'pry'
+require 'csv'
+require 'awesome_print'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
-
+  @@customers = []
 
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = {:street => address[:street], :city => address[:city], :state => address[:state], :zip => address[:zip]}
   end
+
+  def self.all
+    @@customers = CSV.read('data/customers.csv').map {|line| line.to_a}
+    customers.each_with_index do |array, index|
+      id = array[0]
+      email = array[1]
+      address = {
+        :street => array[2],
+        :city => array[3],
+        :state => array[4],
+        :zip => array[5]
+      }
+      @@customers[index] = Customer.new(id, email, address)
+    end
+  end
+
 end
