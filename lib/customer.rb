66430cfa5af6568573198e0f12f015331762a1f7
Amanda Ungco <amanda.ungco@gmail.com>
2018-08-23 23:06:28 -0700

    added class Customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,26 +1,10 @@
 class Customer
-  attr_reader :id :email :delivery_address
-  attr_writer :email :delivery_address
+  attr_reader :id, :email, :address
+  attr_accessor :email, :address
 
-  def initialize(id, email, street, apt, city, state, zip)
-    @id = id #number
-    @email = email#string
-    @delivery_address = {street: street,
-                         appt: ||= nil,
-                         city: city,
-                         state: state,
-                         zip: zip} ## how do you take in the argument and format into hash
+  def initialize(id, email, address)
+    @id = id
+    @email = email
+    @address = address
   end
-
-  def format_delivery_address(delivery_address)
-    delivery_address_array = delivery_address.split(',')
-    @delivery_address[:street] = delivery_address_array(0)
-    @delivery_address[:city] = delivery_address_array(1)
-    @delivery_address[:state] = delivery_address_array(2)
-    @delivery_address[:zip] = delivery_address(3)
-  end
-
 end
-
-amanda = Customer.new(1021, 'amanda.ungco@gmail.com', '410 11th ave, seattle, wa, 98122')
-p amanda.delivery_address
--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,11 @@
 class Order
-  def initialize()
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id
+    @products = {}
+    @customer = customer
+    @fulfillment_status ||= :pending
   end
 
-  attr_reader
-  attr_writer
+  attr_reader :id,
+  attr_accessor :products, :products, :customer, :fulfillment_status
 end
