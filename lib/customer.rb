f1a02f357a2cb448816adcd16d32aec86d47f725
Jazz <jazziesf21@yahoo.com>
2018-08-24 16:33:44 -0700

    Wave 2

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,12 +1,13 @@
 require "csv"
 require "pry"
+require "awesome_print"
 
 class Customer
 
-@@customer = []
-
   attr_reader :id, :email, :address
   attr_writer :email, :address
+
+  @@customer = []
 
   def initialize(id, email, address)
     @id =  id.to_i
@@ -15,18 +16,29 @@ class Customer
   end
 
   def self.all
-  @@customer = CSV.open('data/customers.csv', 'r').map do |line|
+    @@customer = CSV.open('data/customers.csv', 'r').map do |line|
       Customer.new( @id = line[0], @email = line[1],
         @address = {
           street: line[2],
           city: line[3],
           state: line[4],
           zip: line[5]
-        })
-    end
-    return @@customer
+          })
+        end
+        return @@customer
+      end
+
+
+      def self.find(id)
+        self.all
+        return @@customer[id - 1]
+        # @@customer.each do |instance|
+        #   if instance.id == id
+        #     return instance
+        #   elsif instance.id != id
+        #     return nil
+          # end
+      end
+
+
   end
-
-
-
-end
