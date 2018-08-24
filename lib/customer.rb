c4185d66b2bba82cb5755aed7b0fba20689b929e
Sigrid Benezra <sdbenezra@gmail.com>
2018-08-24 15:27:15 -0700

    Add self.all and self.find class methods to customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'pry'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,5 +9,17 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  def self.all
+    @@customer_list = []
+    CSV.read('data/customers.csv').each do |line|
+      @@customer_list << Customer.new(line[0].to_i, line[1], { street: line[2], city: line[3], state: line[4], zip: line[5] })
+    end
+    return @@customer_list
+  end
+
+  def self.find(id)
+    return self.all[id - 1]
   end
 end
