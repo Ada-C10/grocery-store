73ebef8e31431db059daddb0c5f3fc7ce5f51a98
Laura <larmartinez2@gmail.com>
2018-08-24 14:19:08 -0700

    created method that calculates the total for the order, and a method for adding a product to the method

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
   @@valid_statuses = %i[pending paid processing shipped complete] # array of keys
-
+  #order = Order.new(1337, products, customer)
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
@@ -17,6 +19,25 @@ class Order
       else
         raise ArgumentError, "bogus status!"
       end
+  end
+  # products = { "banana" => 1.99, "cracker" => 3.00 }
+  def total
+    prices = @products.values
+    total = prices.sum
+    total_and_tax = total * 0.075 + total
+    total_and_tax = total_and_tax.round(2)
+    if total_and_tax == 0.0
+      return 0
+    else
+      return total_and_tax
+    end
+  end
+# order.add_product("salad", 4.25)
+  def add_product(key, value)
+    if @products.include?(key)
+      raise ArgumentError
+    end
+    @products[key] = value
   end
 
 end
