d2706de1d6ed7f366d073addbf19e460537fc897
Katie Jahanmir <katietj@gmail.com>
2018-08-24 12:52:49 -0700

    added CSV data method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,26 @@
+require 'csv'
 class Customer
   attr_reader :id, :email, :address
   attr_writer :email, :address
+
+@@customers = []
+@@Customerdata = []
 
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = {:street => address[:street] , :city => address[:city], :state => address[:state] , :zip => address[:zip] }
+  end
+
+def load_data
+  @@Customerdata = CSV.open('data/customers.csv', r).map do |line|
+end
+
+  def self.all
+      @@customerdata.each do |customer|
+      @@customers << Customer.new
+    end
+    return @@customers
+end
   end
 end
