07e7277fcefaa0a9786bc9c744ce22485aea4e7a
Daniela Di Iorio <danielad@gmail.com>
2018-08-23 20:39:58 -0700

    Order.all, Order.fill_order_list, Order.extract_products.  Passed given test for first order but needs more testing.

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,11 @@
+require_relative 'customer.rb'
 
 class Order
 
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
+
+  @@order_list
 
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
@@ -13,6 +16,48 @@ class Order
     raise ArgumentError unless valid_statuses.include? fulfillment_status
     @fulfillment_status = fulfillment_status
   end
+
+  def self.extract_products(csv_line)
+    product_hash = {}
+    products = csv_line.clone
+
+    [0, -1, -1].each do |i|
+      products.delete_at(i)
+    end
+
+    products.each do |combined_product_price|
+      individual_product_prices = combined_product_price.split(';')
+      individual_product_prices.each do |product_price|
+        separate = product_price.split(':')
+        product_hash[separate[0]] = separate[1].to_f
+      end
+    end
+
+    return product_hash
+  end
+
+
+  def self.fill_order_list
+    @@order_list = CSV.open("data/orders.csv", "r").map do |line|
+      self.new(line[0].to_i, self.extract_products(line), Customer.find(line[-2].to_i), line[-1].to_sym)
+      #need to parse products into hash
+    end
+  end
+
+  def self.all
+    self.fill_order_list
+    return @@order_list
+  end
+
+  def self.find(id)
+    self.all.each do |order|
+      if order.id == id
+        return order
+      end
+    end
+    return nil
+  end
+
 
   def total
     total = 0.00
@@ -34,3 +79,5 @@ class Order
   end
 
 end
+
+Order.fill_order_list
