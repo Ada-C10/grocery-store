87fe492d92d934aaa4e0cdfa619798208396f4d4
Lindsay Terchin <lterchin@gmail.com>
2018-08-23 21:12:10 -0700

    created method to find the order total

--- lib/order.rb
+++ lib/order.rb
@@ -10,14 +10,27 @@ class Order
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
-        validate_fulfillment_status
+    validate_fulfillment_status
   end
+
 
   def validate_fulfillment_status
     fulfillment_status_options = [:pending, :paid, :processing, :shipped, :complete]
     if fulfillment_status_options.include?(fulfillment_status) == false
-      raise ArgumentError.new("Fulfillment status :pending, :paid, :processing, :shipped, or :complete")
+      raise ArgumentError.new("Fulfillment status must be :pending, :paid, :processing, :shipped, or :complete")
     end
+  end
+
+
+  def total
+    total_cost = 0
+
+    @products.each do |product, cost|
+      total_cost += cost
+    end
+
+    total = total_cost + (total_cost * 0.075)
+    return total.round(2)
   end
 
 end
