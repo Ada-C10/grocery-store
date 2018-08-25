96e917996e7ad37d8b8aef68cb00815974e0596d
Katricia Smith <smithkatricia@gmail.com>
2018-08-25 12:55:28 -0700

    Moved CSV open to end of class + moved array population outside of constructor

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,28 +1,39 @@
 require 'csv'
-
-CSV.open("data/customers.csv",'r').each do |line|
-  puts line
-end
+require 'pry'
+# @@customers_array = CSV.read("data/customers.csv")
 
 class Customer
   attr_reader :id
-
   attr_accessor :email, :address
 
+  @@customers = []
+
   def initialize(id, email, address)
-    @id = id
+    @id = id.to_i
     @email = email
     @address = address
   end
 
+  def add_to_customers
+    @@customers << self
+  end
+
   def self.all
-    puts self
+    @@customers
   end
 
   def self.find(id)
-    if @id == Customer.id
-      return @customer
+    if id == Customer.id
+      return self
     end
   end
 
+end
+
+# csv_array = []
+CSV.open("data/customers.csv",'r').each do |line|
+  address = {street: line[2], city: line[3], state: line[4], zip: line[5]}
+  cust = Customer.new(line[0], line[1], address)
+  cust.add_to_customers
+    # Customer.new(line[0], line[1], {street: line[2], city: line[3], state: line[4], zip: line[5]})
 end
