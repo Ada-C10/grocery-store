40e280f6efe36699244f4e0ed9c68b085510d2d9
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-08-25 08:25:26 -0700

    Add tests and optional Order.find_by_customer method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,7 @@
+require "csv"
+require "ap"
+require "pry"
+
 class Customer
 
   attr_reader :id
@@ -12,11 +16,22 @@ class Customer
   end
 
   def self.all
+    @@customer = []
+    CSV.foreach("data/customers.csv") do |customer|
+      address = {
+        street: customer[2],
+        city: customer[3],
+        state: customer[4],
+        zip: customer[5].strip,
+      }
+      @@customer << Customer.new(customer[0].to_i, customer[1], address)
+    end
     return @@customer
   end
 
-  def self.find(id)
-    return @@customer.find {|cust| cust.id == id}
+  def self.find(num)
+    @@customer = Customer.all
+    return @@customer.find {|customer| customer.id == num}
   end
 
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,14 @@
+require 'csv'
+require 'ap'
+require_relative 'customer'
+require 'ap'
+
 class Order
 
   attr_reader :id, :customer
   attr_accessor :products, :fulfillment_status
+
+  @@orders = []
 
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
@@ -28,14 +35,35 @@ class Order
     @products.delete(product_name)
   end
 
+  def self.all
+    @@orders = []
+    CSV.foreach("data/orders.csv") do |order|
+      customer_products = order[1].split(";")
+      product_hash = {}
+      customer_products.each do |item|
+        temp = item.split(":")
+        product_hash[temp[0]] = temp[1].to_f
+      end
+      @@orders << Order.new(order[0].to_i, product_hash, Customer.find(order[2].to_i), order[3].to_sym)
+    end
+
+    return @@orders
+  end
+
+  def self.find(id)
+    @@orders = Order.all
+    return @@orders.find {|order| order.id == id}
+  end
+
+  def self.find_by_customer(customer_id)
+    orders = Order.all
+    list = orders.find_all {|order| order.customer.id == customer_id}
+    return list if !list.empty?
+    return nil
+  end
+
 end
 
-#test = Order.new(2452, {"apples" => 1}, "Anna", :blah)
-
-#
-# products = { "banana" => 1.99, "cracker" => 3.00 }
-#
-# order = Order.new(1337, products, "x")
-# puts order.products
-# order.remove_product("cracker")
-# puts order.products
+# orders = Order.all
+# puts "#{orders.class}"
+# puts "#{orders.length}"
