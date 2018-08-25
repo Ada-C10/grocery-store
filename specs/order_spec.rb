19fdc52549da7662a6c8cfcb2582582979b7acc0
Katie Jahanmir <katietj@gmail.com>
2018-08-25 08:41:52 -0700

    wrote all tests

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -118,7 +118,7 @@ describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
       # TODO: Your test code here!
-      orders = Orders.all
+      orders = Order.all
 
       expect(orders.length).must_equal 100
       orders.each do |o|
@@ -148,20 +148,31 @@ describe "Order Wave 2" do
 
     xit "Returns accurate information about the last order" do
       # TODO: Your test code here!
+      last = Order.all.last
+      expect(last.id).must_equal 100
     end
   end
 
   xdescribe "Order.find" do
     xit "Can find the first order from the CSV" do
       # TODO: Your test code here!
+      first = Order.find(1)
+
+      expect(first).must_be_kind_of Order
+      expect(first.id).must_equal 1
     end
 
     xit "Can find the last order from the CSV" do
       # TODO: Your test code here!
+      last = Order.find(100)
+
+      expect(last).must_be_kind_of Order
+      expet(last.id).must_equal 100
     end
 
     xit "Returns nil for an order that doesn't exist" do
       # TODO: Your test code here!
+      expect(Order.find(1243)).must_be_nil
     end
   end
 end
