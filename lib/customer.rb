b1b6c57f6e5771c2b4ef5c5a9ab96f3cb738aa17
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-08-25 13:55:14 -0700

    find method created, invokes self.all and passes tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,7 +6,6 @@ class Customer
   attr_reader :id
   attr_accessor :email, :address
 
-  @all_customers = []
   def initialize(id, email, address)
     @id = id
     @email = email
@@ -14,36 +13,43 @@ class Customer
   end
 
   def self.all
+    all_customers = []
     # returns a collection of Customer instances, representing all Customers in csv
-    all_customers = CSV.open('data/customers.csv', 'r').map do |line|
+    customers = CSV.open('data/customers.csv', 'r').map do |line|
       line
     end
 
-    all_customers.each do |cust|
+    customers.each do |cust|
       #binding.pry
       id = cust[0].to_i
       email = cust[1]
-      address = {street: cust[2], city: cust[3], state: cust[4], zip: cust[5]}
-      customer = Customer.new(id, email, address)
-      @all_customers << customer
+      address = { street: cust[2], city: cust[3], state: cust[4], zip: cust[5] }
+      # add each current instance to the all_customers array
+      all_customers << self.new(id, email, address)
+      #binding.pry
     end
-    #binding.pry
-    return @all_customers
-    # TODO: is it possible to DRY this up where CSV gets imported and each line is read into
-    #      class instance of all customers array?????
+
+    # binding.pry
+    return all_customers
+    # TODO: Is it possible to DRY this up where CSV gets imported and each line
+    #       is read into class instance of all customers array?????
+
   end
 
   def self.find(id)
     # returns an instance of Customer where the value matches the id parameter
     # provided !!invokes Customer.all and search through results for matching id
+    found = Customer.all.find { |cust| cust.id == id }
+
+    return found
   end
 
-  def load_data(filename)
-    olympic_data = CSV.open(filename, 'r', headers: true).map do |item|
-      item.to_h
-    end
+  # def load_data(filename)
+  #   olympic_data = CSV.open(filename, 'r', headers: true).map do |item|
+  #     item.to_h
+  #   end
     # p olympic_data.class
-    return olympic_data
+    # return olympic_data
 
-  end
+  # end
 end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -6,7 +6,7 @@ require_relative '../lib/customer'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-xdescribe "Customer Wave 1" do
+describe "Customer Wave 1" do
   ID = 123
   EMAIL = "a@a.co"
   ADDRESS = {
@@ -16,7 +16,7 @@ xdescribe "Customer Wave 1" do
     zip: "98101"
   }.freeze
 
-  xdescribe "#initialize" do
+  describe "#initialize" do
     it "Takes an ID, email and address info" do
       cust = Customer.new(ID, EMAIL, ADDRESS)
 
@@ -37,7 +37,7 @@ describe "Customer Wave 2" do
   describe "Customer.all" do
     it "Returns an array of all customers" do
       customers = Customer.all
-
+      p customers.length
       expect(customers.length).must_equal 35
       customers.each do |c|
         expect(c).must_be_kind_of Customer
@@ -46,31 +46,33 @@ describe "Customer Wave 2" do
 
     it "Returns accurate information about the first customer" do
       first = Customer.all.first
+      # p "first: #{first.id}"
       expect(first.id).must_equal 1
     end
 
     it "Returns accurate information about the last customer" do
       last = Customer.all.last
+      # p "last: #{last.id}"
       expect(last.id).must_equal 35
     end
   end
 
   describe "Customer.find" do
-    xit "Can find the first customer from the CSV" do
+    it "Can find the first customer from the CSV" do
       first = Customer.find(1)
 
       expect(first).must_be_kind_of Customer
       expect(first.id).must_equal 1
     end
 
-    xit "Can find the last customer from the CSV" do
+    it "Can find the last customer from the CSV" do
       last = Customer.find(35)
 
       expect(last).must_be_kind_of Customer
       expect(last.id).must_equal 35
     end
 
-    xit "Returns nil for a customer that doesn't exist" do
+    it "Returns nil for a customer that doesn't exist" do
       expect(Customer.find(53145)).must_be_nil
     end
   end
