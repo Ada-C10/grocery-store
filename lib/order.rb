5be9744ec0623b9cdb1f30119a53f6dc03d06006
Katrina Aganon <katrina.aganon@gmail.com>
2018-08-25 16:46:55 -0700

    used test code on the bottom of file, code is working

--- lib/order.rb
+++ lib/order.rb
@@ -96,20 +96,20 @@ class Order
 end
 
 
-
-address = {
-  street: "123 Main",
-  city: "Seattle",
-  state: "WA",
-  zip: "98101"
-}
-customer = Customer.new(123, "a@a.co", address)
-products = { "banana" => 1.99, "cracker" => 3.00 }
-order = Order.new(1337, products, customer)
-order.add_product("apple", 9.99)
-puts order.products
-order.remove_product("banana")
-puts order.products
-order.remove_product("apple")
-puts order.products
-order.remove_product("apple")
+#
+# address = {
+#   street: "123 Main",
+#   city: "Seattle",
+#   state: "WA",
+#   zip: "98101"
+# }
+# customer = Customer.new(123, "a@a.co", address)
+# products = { "banana" => 1.99, "cracker" => 3.00 }
+# order = Order.new(1337, products, customer)
+# order.add_product("apple", 9.99)
+# puts order.products
+# order.remove_product("banana")
+# puts order.products
+# order.remove_product("apple")
+# puts order.products
+# order.remove_product("apple")
