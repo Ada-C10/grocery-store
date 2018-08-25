df800704c700a385c988f30b3990c789292875a8
Katrina Aganon <katrina.aganon@gmail.com>
2018-08-25 12:04:51 -0700

    created self.all of customer instances from csv file

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+require 'awesome_print'
 require 'pry'
 
 class Customer
@@ -10,4 +12,37 @@ attr_accessor :email, :address
     @address = address
   end
 
+  def self.all
+    customers = CSV.read('data/customers.csv').map do |customer|
+      customer
+    end
+
+
+    all_customers = customers.map do |customer|
+      @id = customer[0]
+      @email = customer[1]
+      @address = {:street => customer[2], :city => customer[3], :state => customer[4], :zip => customer[5]}
+      Customer.new(@id, @email, @address)
+    end
+
+    # binding.pry
+    return all_customers
+
+  end
+
+  def self.find(id)
+    Customer.all
+
+    return
+  end
+
 end
+
+# address = {
+#   street: "123 Main",
+#   city: "Seattle",
+#   state: "WA",
+#   zip: "98101"
+# }
+# customer = Customer.new(123, "a@a.co", address)
+puts Customer.all.class
