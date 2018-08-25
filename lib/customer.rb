f5babd0cf6dfa0ba37ee1614bf8f4afa3d7a1a30
Hannah <hannahvanseeters@gmail.com>
2018-08-25 08:58:00 -0700

    complete up until last question in wave 2

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,47 @@
+require 'csv'
+require 'awesome_print'
+require_relative 'order'
+
 class Customer
   attr_accessor :email, :address
   attr_reader :id
 
-  def initialize
+  # or address could be {:name, :address}
+  def initialize(id, email, address)
     @id = id
     @email = email
-    @address = {}
+    @address = address
+    # {name: name, street: street, city: city, zipcode: zipcode}
+    # @@instances << self
   end
-end 
+
+  def self.all
+    total_data = CSV.open("../data/customers.csv", 'r').map do |line|
+      Customer.new(line[0].to_i, line[1], {street: line[2],city: line[3],state: line[4],zip: line[5]})
+    end
+
+    return total_data
+    # @@instances.inspect
+  end
+ # params integer
+ # returns instance of Customer
+  def self.find(id)
+    self.all.find { |x| x.id == id }
+  end
+
+
+end
+
+# ap Customer.find(36)
+
+# ap Customer.all
+# or
+# def initialize
+  # @id
+  # @email
+  # @name
+  # @street
+  # @city
+  # @zip
+  # @ address = {name: @name, street: @street, city: @city, zipcode: @zipcode}
+# end
--- lib/order.rb
+++ lib/order.rb
@@ -1,50 +1,69 @@
-class Order(id, products, customer, fulfillment_status)
+require 'csv'
+require 'awesome_print'
+require_relative 'customer'
+
+class Order
   # should products be only read only?
   # is product list their order or what they are choosing from?
-  attr_reader :id, :products
-  attr_accessor :fulfillment_status
+  attr_reader :id, :products, :customer, :fulfillment_status
   #accept products as a hash? or do @products = {}
   #write fulfillement status like below, or put keyword argument into Class arguements
   #fulfillment_status: pending
-  def initialize
+  def initialize(id, products, customer, fulfillment_status= :pending)
     @id = id
-    @products = {}
-    @fulfillment_status ||= :pending
+    @products = products
+    # customer is integer
+    @customer = customer
+    status_options = [:pending, :paid, :processing, :shipped, :complete]
+    if status_options.include?(fulfillment_status)
+      @fulfillment_status = fulfillment_status
+    else
+      raise ArgumentError.new("This is not a valid status.")
+    end
+
   end
   #wants <#93r9usdlkfja4nf I believe
+  # params integer
+  # returns integer
+  # should return customer object
   def customer_info(customer)
     return customer
   end
 
-  def fulfillment_status
-    status_options = [:pending, :paid, :processing, :shipped, :complete]
-    # if status is included, then return status
-    status_options.each do |status|
-      if @fulfillment_status == status
-        return @fulfillment_status
-      end
-    end
-    # otherwise raise ArgumentError
-    raise ArgumentError.new("This is not a valid status.")
-  end
-
   def total
     #sum products
-    sum = @products.values.reduce(:+)
-
+    #TODO: could make this cleaner(more concise) probably.
+    if @products.empty?
+      return 0
+    else
+      sum = @products.values.reduce(:+)
+    end
     #add sales tax
     sales_tax_total = (sum * 1.075).round(2)
     return sales_tax_total
   end
 
   def add_product(product_name, price)
-    @products.map do |k, v|
-      if product_name = k
+    @products.each do |k, v|
+      if product_name == k
         raise ArgumentError.new("This product already exists")
-      else
-        @products[product_name] = price
       end
-    end 
+    end
+    @products[product_name] = price
+  end
+
+  def self.all
+    total_orders = CSV.open('../data/orders.csv', 'r').map do |line|
+      products = line[1].split(/[;:]/)
+      # changing array into hash
+      # making values all floats
+      products_hash = Hash[*products].map { |k,v| [k, v.to_f] }.to_h
+      Order.new(line[0].to_i, products_hash, line[2].to_i, line[3].to_sym)
+    end
+
+    return total_orders
   end
 
 end
+
+ap Order.customer_info(20)
