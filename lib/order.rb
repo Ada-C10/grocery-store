c7174f1d7127dcc5def00a582e84b841fb534b7a
Valerie Gidzinski <valgidzi@gmail.com>
2018-08-25 14:01:32 -0700

    Update Order class constructor method -add fulfillment_status parameter conditions

--- lib/order.rb
+++ lib/order.rb
@@ -2,12 +2,18 @@ class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, products, customer, fulfillment_status)
+  VALID_STATUSES = [:pending, :paid, :processing, :shipped, :complete]
+
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = {}
     @customer = customer
     @fulfillment_status = fulfillment_status
+
+
+    unless VALID_STATUSES.include? (fulfillment_status)
+      raise ArgumentError
+    end
   end
-
-
+  
 end
