d9ed6a4e13dd7c490eed507b9147ad3b31e40907
Amber Lynn <griffiam@gmail.com>
2018-08-25 12:01:00 -0700

    added a raise argumenterror, calculated total iterating over hash and assigning value to a variable with a tax amount

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,24 +1,30 @@
-require_relative 'order'
+require_relative '../lib/order'
 
 class Customer
   attr_reader :id
   attr_accessor :email, :address
+  ID = 0
+  EMAIL = ""
+  ADDRESS = {}
 
-  def initialize(ID, EMAIL, ADDRESS)
-    @ID = ID
+  def initialize(id, email, address)
+    @id = id
+    @email = email
+    @address = address
     # if @ID.class != integer
     #   puts "Not a num"
     # end
+  end
 
-    @EMAIL = EMAIL
-    # if @EMAIL.class != string
-    #   puts "not a string"
-    # end
 
-    @ADDRESS = ADDRESS
-    # if @ADDRESS.class != hash
-    #   puts "not a hash of address data"
-    # end
+  # if @EMAIL.class != string
+  #   puts "not a string"
+  # end
+
+
+  # if @ADDRESS.class != hash
+  #   puts "not a hash of address data"
+  # end
   # end
 
 
--- lib/order.rb
+++ lib/order.rb
@@ -1,28 +1,43 @@
-require_relative 'customer'
-
+#require_relative '..lib/customer'
 class Order
   attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, product_cost, customer, status=':pending')
+  def initialize(id, products, customer, fulfillment_status=:pending)
     @id = id
     # if @id.class != integer
     #   puts "not a num"
     # end
 
-    @product_cost = product_cost
+    @products = products
     # if @product_cost.class != hash
     #   puts "not a hash"
     # end
 
     @customer = customer
 
-    @status = status
-    # if @status.class != string
-    #   puts "not a string"
-    # end
+    @fulfillment_status = fulfillment_status
+
+    raise ArgumentError unless
+    @fulfillment_status == :pending || @fulfillment_status == :paid || @fulfillment_status == :processing || @fulfillment_status == :shipped || @fulfillment_status == :complete
   end
 
-  def total()
+  # def add_product(name, price)
+  #   raise ArgumentError unless
+  #   name != @product.keys
+  # end
+  #   @products[name] = price
+  # end
 
+def total
+  expected_total = 0.00
+  tax = 7.5 / 100
+  @products.each_value do |value|
+    expected_total += value
   end
+  expected_total = (expected_total * tax) + expected_total
+  return expected_total.round(2)
+end
+
+
 end
