68e488abf6fe8000a2c355b43a436463923bb706
BarbaraWidjono <barbarawidjono@gmail.com>
2018-08-24 22:11:09 -0700

    Added partially functional self.all

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'awesome_print'
+
 class Order
 
 attr_reader :id
@@ -56,4 +59,43 @@ def add_product(product_name, price)
 
 end
 
+# returns a collection of Order instances, representing all of the Orders described in the CSV file
+def self.all
+  all_order_instances = []
+
+  CSV.open("data/orders.csv", "r").map do |line|
+    product_hash = {}
+    id = line[0].to_i
+    all_products = line[1]
+    all_products_into_array = all_products.split(";")
+
+    index = 0
+    while index < all_products_into_array.length
+      each_product_own_array = all_products_into_array[index].split(":")
+      product_hash[each_product_own_array[0]] = [each_product_own_array[1]]
+
+      index += 1
+    end
+
+    customer_id = line[2].to_i
+    status = line[3].to_sym
+
+    new_order = Order.new(id, product_hash, customer_id, status)
+    all_order_instances << new_order
+  end
+
+  return all_order_instances
+end
+
+end
+
+# TEST
+run_method = Order.all
+puts "#{run_method}"
+
+index = 0
+while index < run_method.length
+  x = run_method[index]
+  puts "Order num: #{x.id} | products: #{x.products} | customer: #{x.customer} | status: #{x.fulfillment_status}"
+  index += 1
 end
