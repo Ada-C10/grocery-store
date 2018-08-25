a567e6d569777c9ff3e47f389ed71115b2941b8c
Lindsay Terchin <lterchin@gmail.com>
2018-08-25 11:57:55 -0700

    changed all_customers from a class variable to a local variable

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,32 +5,16 @@ class Customer
   attr_reader :id
   attr_accessor :email, :address, :street, :city, :state, :zip
 
-  @@all_customers = []
-
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
-    @@all_customers << self
+
   end
 
-  #
-  # def self.create_address_hash
-  #   CSV.open("data/customers.csv", "r").each do |line|
-  #     index = 0
-  #     @address_hash = {}
-  #     @address_hash[:street] = line[2]
-  #     @address_hash[:city] = line[3]
-  #     @address_hash[:state] = line[4]
-  #     @address_hash[:zip] = line[5]
-  #
-  #   end
-  #   return @address_hash
-  # end
 
-
-  def self.transform_csv
-    @customer_data = []
+  def self.all
+    all_customers = []
     CSV.open("data/customers.csv", "r").each do |line|
 
       @address_hash = {}
@@ -39,41 +23,49 @@ class Customer
       @address_hash[:state] = line[4]
       @address_hash[:zip] = line[5]
 
-      customer_data_hash = {
-        id: line[0],
-        email: line[1],
-        address: @address_hash
-      }
+      x = Customer.new(line[0].to_i, line[1], @address_hash)
 
-      @customer_data << customer_data_hash
+      all_customers << x
     end
-    return @customer_data
+    return all_customers
   end
 
+  end
+
+
+
+##FIRST TRY
   # def self.transform_csv
   #   @customer_data = []
   #   CSV.open("data/customers.csv", "r").each do |line|
   #
-  #     customer_data_hash = {
+  #     @address_hash = {}
+  #     @address_hash[:street] = line[2]
+  #     @address_hash[:city] = line[3]
+  #     @address_hash[:state] = line[4]
+  #     @address_hash[:zip] = line[5]
+  #
+  #     @customer_data_hash = {
   #       id: line[0],
   #       email: line[1],
-  #       address: self.create_address_hash
+  #       address: @address_hash
   #     }
   #
-  #     @customer_data << customer_data_hash
+  #     @customer_data << @customer_data_hash
   #   end
   #   return @customer_data
   # end
 
 
-  def self.all
-    self.transform_csv
-    index = 0
-    @customer_data.each do
-      Customer.new(@customer_data[index][:id], @customer_data[index][:email], @customer_data[index][:address])
-      index += 1
-    end
-    return @@all_customers
-  end
-
-end
+##FIRST TRY
+#   def self.all
+#     self.transform_csv
+#     index = 0
+#     @customer_data.each do
+#       Customer.new(@customer_data[index][:id], @customer_data[index][:email], @customer_data[index][:address])
+#       index += 1
+#     end
+#     return @@all_customers
+#   end
+#
+# end
