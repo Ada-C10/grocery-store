29432b76fb4188856c245d4a7f43078ec05320f8
Sigrid Benezra <sdbenezra@gmail.com>
2018-08-26 16:01:15 -0700

    Complete self.all & self.find methods in order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'pry'
+
 class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
@@ -12,16 +15,40 @@ class Order
   end
 
   def total
-    product_sum = products.values.sum
+    product_sum = @products.values.sum
     sum_plus_tax = product_sum * 1.075
     return sum_plus_tax.round(2)
   end
 
   def add_product(name, price)
-    if products.keys.include?(name)
+    if @products.keys.include?(name)
       raise ArgumentError, "Duplicate product"
     else
-      products[name] = price
+      @products[name] = price
     end
+  end
+
+  def self.product_parse(products)
+    product_hash = {}
+    products_split = products.split(';')
+    products_split.each do |product|
+      product_array = product.split(":")
+      product_hash[product_array[0]] = product_array[1].to_f
+    end
+    # binding.pry
+    return product_hash
+  end
+
+  def self.all
+    order_list = []
+    CSV.read('data/orders.csv').each do |line|
+      product = Order.product_parse(line[1])
+      order_list << Order.new(line[0].to_i, product, Customer.find(line[2].to_i), line[3].to_sym)
+    end
+    return order_list
+  end
+
+  def self.find(id)
+    return self.all[id - 1]
   end
 end
