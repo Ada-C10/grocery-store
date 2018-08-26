5417a8dd88611f637d71ba584ca85d1c002746f5
Lindsay Terchin <lterchin@gmail.com>
2018-08-26 12:40:04 -0700

    wrapped customer.rb in a module

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,80 +1,46 @@
 require 'csv'
 
-class Customer
+module CustomerInfo
 
-  attr_reader :id
-  attr_accessor :email, :address, :street, :city, :state, :zip
+  class Customer
 
-  def initialize(id, email, address)
-    @id = id
-    @email = email
-    @address = address
-  end
+    attr_reader :id
+    attr_accessor :email, :address, :street, :city, :state, :zip
 
-
-  def self.all
-    all_customers = []
-    CSV.open("data/customers.csv", "r").each do |line|
-
-      @address_hash = {}
-      @address_hash[:street] = line[2]
-      @address_hash[:city] = line[3]
-      @address_hash[:state] = line[4]
-      @address_hash[:zip] = line[5]
-
-      new_customer = Customer.new(line[0].to_i, line[1], @address_hash)
-
-      all_customers << new_customer
+    def initialize(id, email, address)
+      @id = id
+      @email = email
+      @address = address
     end
-    return all_customers
-  end
 
 
-  def self.find(id)
-    Customer.all.each do |customer|
-      if customer.id == id
-        return customer
+    def self.all
+      all_customers = []
+      CSV.open("data/customers.csv", "r").each do |line|
+
+        @address_hash = {}
+        @address_hash[:street] = line[2]
+        @address_hash[:city] = line[3]
+        @address_hash[:state] = line[4]
+        @address_hash[:zip] = line[5]
+
+        new_customer = Customer.new(line[0].to_i, line[1], @address_hash)
+
+        all_customers << new_customer
       end
+      return all_customers
     end
-    return nil
+
+
+    def self.find(id)
+      Customer.all.each do |customer|
+        if customer.id == id
+          return customer
+        end
+      end
+      return nil
+    end
+
   end
 
 end
-
-
-
-##FIRST TRY
-  # def self.transform_csv
-  #   @customer_data = []
-  #   CSV.open("data/customers.csv", "r").each do |line|
-  #
-  #     @address_hash = {}
-  #     @address_hash[:street] = line[2]
-  #     @address_hash[:city] = line[3]
-  #     @address_hash[:state] = line[4]
-  #     @address_hash[:zip] = line[5]
-  #
-  #     @customer_data_hash = {
-  #       id: line[0],
-  #       email: line[1],
-  #       address: @address_hash
-  #     }
-  #
-  #     @customer_data << @customer_data_hash
-  #   end
-  #   return @customer_data
-  # end
-
-
-##FIRST TRY
-#   def self.all
-#     self.transform_csv
-#     index = 0
-#     @customer_data.each do
-#       Customer.new(@customer_data[index][:id], @customer_data[index][:email], @customer_data[index][:address])
-#       index += 1
-#     end
-#     return @@all_customers
-#   end
-#
-# end
