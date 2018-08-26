381a0bc7813052a28f571d477103e83f19447337
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-26 08:43:33 -0700

    Define total method with passing tests

--- lib/order.rb
+++ lib/order.rb
@@ -3,14 +3,21 @@ class Order
   attr_accessor :products, :fulfillment_status
 
   def initialize(id, products, customer, fulfillment_status = :pending)
-    
+
     valid_statuses = %i[pending paid processing shipped complete]
 
     @id = id
-    @products = {}
+    @products = products
     @customer = customer
 
     raise ArgumentError unless valid_statuses.include?(fulfillment_status)
     @fulfillment_status = fulfillment_status
   end
+
+  def total
+    subtotal = @products.values.sum
+    total = 1.075 * subtotal
+    return total.round(2)
+  end
+
 end
