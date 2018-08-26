64a8007642b388945eab4a577b6ecb9affb85e12
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-26 09:52:29 -0700

    Write self all and find methods with passing tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,5 +8,19 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  def self.all
+    all_customers = []
+    CSV.read("../data/customers.csv").each do |customer_info|
+      all_customers << Customer.new(customer_info[0].to_i, customer_info[1], customer_info[2])
+    end
+    return all_customers
+  end
+
+  def self.find(id)
+    all_customers = self.all
+    found_customer = all_customers.find { |customer| customer.id == id }
+      return found_customer
   end
 end
