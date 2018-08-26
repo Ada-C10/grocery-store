8e78dbd8cfc65e016ac554553fa761493dc38181
Addison Cole <addison.cole85@gmail.com>
2018-08-25 20:25:12 -0700

    refactored Customer.find, was returning incorrect values for middle entries of CSV

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,36 +11,31 @@ class Customer
     @address = address
   end
 
-def self.all
-  @customers = []
-  CSV.open("./data/customers.csv", "r").each do |row|
-    id = row[0].to_i
-    email = row[1].to_s
-    address = {
-      street: row[2].to_s,
-      city: row[3].to_s,
-      state: row[4].to_s,
-      zip: row[5].to_s
-    }
-    customer = Customer.new(id, email, address)
-    @customers << customer
-  end
-  return @customers
-end
-
-def self.find(id)
-  @customers = Customer.all
-  last = @customers.last
-  if last.id == id
-    return last
-  end
-  @customers.each do |customer|
-    if customer.id == id
-      return customer
-    else
-      return nil
+  def self.all
+    @customers = []
+    CSV.open("./data/customers.csv", "r").each do |row|
+      id = row[0].to_i
+      email = row[1].to_s
+      address = {
+        street: row[2].to_s,
+        city: row[3].to_s,
+        state: row[4].to_s,
+        zip: row[5].to_s
+      }
+      customer = Customer.new(id, email, address)
+      @customers << customer
     end
+    return @customers
   end
-end
 
+  def self.find(id)
+    @customers = Customer.all
+    # testing_id = id
+    @customers.each do |customer|
+      if customer.id == id
+        return customer
+      end
+    end
+    return nil
+  end
 end
