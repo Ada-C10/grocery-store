bd39ab71a39937d335d5e5eca18f7e375c4b5ecf
Melissa O'Hearn <melissa.ohearn@gmailcom>
2018-08-26 15:28:45 -0700

    stated wave two, imported csv file and created a new instance of all customers

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,13 @@
+require 'csv'
+
 
 class Customer
 
 # helper mothods: automatically add reader and writer methods for these varibales. attr_accessor allows for write and read access
 attr_reader :id
 attr_accessor :email, :address
+@@all
+@@find
 
 # constructor: this will automatically called wen Customer.new is invoked
   def initialize(id, email, address)
@@ -12,11 +16,21 @@ attr_accessor :email, :address
     @email = email
     @address = address
   end
+  # class methods
+  def self.all
+    # inport csv file, into customers_arry. map over that array to create a new instance of Customer. Return the instances of all customers
+    customers_array = CSV.read("data/customers.csv")
+    all_customers = customers_array.map do |customer|
+      Customer.new(customer[0].to_i, customer[1], customer[2])
+    end
+    return all_customers
 
-# def customer
-#   return
-# end
+  end
 
-# instance method. invoked using . notation outside class
+  def self.find(id)
+    return @@find
+  end
+
+
 
 end
