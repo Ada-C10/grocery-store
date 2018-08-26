97e70deef0f4b3a3d48c261961aec7adc41aa6aa
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-08-25 20:11:39 -0700

    wave 2 finished

--- lib/order.rb
+++ lib/order.rb
@@ -1,11 +1,12 @@
 require 'pry'
+require_relative 'customer'
 
 class Order
   attr_reader :id, :customer, :products, :fulfillment_status
 
   def initialize(id, products, customer, fulfillment_status = :pending)
     valid_statuses = %i[pending paid processing shipped complete]
-    print "#{valid_statuses}"
+
     if valid_statuses.include?(fulfillment_status)
       @fulfillment_status = fulfillment_status
     else
@@ -36,6 +37,50 @@ class Order
       @products.delete(prd_name)
     else
       raise ArgumentError,"Product namne does not exist"
+    end
+  end
+
+
+
+  def self.all
+    all_orders = []
+
+    CSV.open('data/orders.csv', 'r').map do |line|
+      order_info = line.to_a
+      order_id = order_info[0].to_i
+      fulfillment_status = order_info[-1].to_sym
+
+      customer_id = order_info[-2].to_i
+      customer = Customer.find(customer_id)
+
+      prod_info = order_info[1].split(';')
+      product_h = {}
+
+      prod_info.each do |prod|
+        prod_new = prod.split(':')
+        product_h[prod_new.first] = prod_new.last.to_f
+      end
+
+      new_order = self.new(order_id, product_h, customer, fulfillment_status)
+      all_orders.push(new_order)
+    end
+
+    return all_orders
+  end
+
+  def self.find(id)
+    all_orders = self.all
+    order_found = all_orders.select {|order| order.id == id}
+    return order_found[0]
+  end
+
+  def self.find_by_customer(customer_id)
+    all_orders = self.all
+    order_found = all_orders.select {|order| order.customer.id == customer_id}
+    if order_found == []
+      return nil
+    else
+      return order_found
     end
   end
 end
