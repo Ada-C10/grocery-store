dd52a6de4fd14f5296778779b9f4b9c1a325af28
Valerie Gidzinski <valgidzi@gmail.com>
2018-08-25 19:59:54 -0700

    Fix Customer class whitespace

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,19 +11,26 @@ class Customer
     @address = address
   end
 
+
   def self.all
     customers = []
+
     CSV.read('data/customers.csv').each do |customer|
+
       id = customer[0].to_i
       email = customer[1]
       address = {street: customer[2], city: customer[3], state: customer[4], zip: customer[5]}
+
       customers << Customer.new(id, email, address)
     end
+
     return customers
   end
 
+
   def self.find(id)
     customers = Customer.all
+
     customers.find do |customer|
       customer.id == id
     end
