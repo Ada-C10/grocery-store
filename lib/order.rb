0429a97b1b5393e0c147e3cef4e5818a650cdb32
Divya <divyamohanan@gmail.com>
2018-08-26 11:52:52 -0700

    Wave 1 code seems to work

---
+++ lib/order.rb
@@ -0,0 +1,59 @@
+require 'csv'
+require 'awesome_print'
+filename = "data/orders.csv"
+class Order
+  #
+  filename = "data/orders.csv"
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    #CSV.read(filename,'r').map do |line|
+      #if line[0] == order_id
+        @id = id
+        if products.keys == {}
+          @products = 0
+        end
+        @products = products
+        @customer = customer
+        if fulfillment_status != :pending && fulfillment_status != :paid && fulfillment_status != :processing && fulfillment_status != :shipped && fulfillment_status != :complete
+          raise ArgumentError, "No status provided"
+        else
+          @fulfillment_status = fulfillment_status
+        end
+
+        # array = products.split(';')
+        # hash = {}
+        #   array.each do |item|
+        #     hash ["#{item.split(':').first}"] = (item.split(':').last).to_f
+        #   end
+        # @products = hash
+      #end
+    #end
+  end
+  attr_reader :id, :products, :customer, :fulfillment_status
+
+  def total
+    array = @products.values
+    if array != []
+      tally = array.reduce(:+)
+      tally += (0.075 * tally)
+      return tally.round(2)
+    else
+      return 0
+    end
+  end
+
+  def add_product(product_name, price)
+    if (@products).include?(product_name)
+
+      raise ArgumentError, "This item has already been added"
+    end
+    @products["#{product_name}"] = price
+  end
+end
+# order = Order.new(1337, {"banana" => 1.99, "cracker" => 3.00}, 22)
+#
+# order.add_product("sandwich", 4.25)
+# order.products.include?("sandwich")
+#testrun = Order.new(20,{},22)
+
+ #testrun.add_product("Bananas", 20.0)
+# testrun.total
