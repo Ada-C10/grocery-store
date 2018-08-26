4ffcd0c7eda23fcb5d9138c4484ea3828e7c79d1
Semret Nicodimos <snicodimos@gmail.com>
2018-08-26 15:36:06 -0700

    Added new test for optional parts

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -111,9 +111,49 @@ describe "Order Wave 1" do
       expect(order.total).must_equal before_total
     end
   end
+
+  describe "#remove_product" do
+    it "Decreases the number of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      before_count = products.count
+      order = Order.new(1337, products, customer)
+
+      order.remove_product("banana")
+      expected_count = before_count - 1
+      expect(order.products.count).must_equal expected_count
+    end
+
+    it "Is removed from the collection of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Order.new(1337, products, customer)
+
+      order.remove_product("banana")
+      expect(order.products.include?("banana")).must_equal false
+    end
+
+    it "Raises an ArgumentError if the product is not present" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+
+      order = Order.new(1337, products, customer)
+
+      expect {
+        order.remove_product("mango")
+      }.must_raise ArgumentError
+
+    end
+
+    it "Raises an ArgumentError if the collection is empty" do
+      products = {}
+      order = Order.new(1337, products, customer)
+
+      expect {
+        order.remove_product("banana")
+      }.must_raise ArgumentError
+    end
+  end
 end
 
-# TODO: change 'xdescribe' to 'describe' to run these tests
+
 describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
