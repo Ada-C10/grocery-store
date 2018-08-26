224905041346ce20a12c7566ab61a3e7e68462ec
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-08-26 08:08:03 -0700

    Removed old comments/test code.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,3 @@
-# Create a class called `Customer`. Each new Customer should include the following attributes:
-# - ID, a number
-# - Email address, a string
-# - Delivery address, a hash
-#
-# ID should be _readable_ but not _writable_; t
-# he other two attributes can be both read and written.
 
 require 'pry'
 require 'csv'
@@ -13,24 +6,20 @@ class Customer
   attr_reader :id
   attr_accessor :email, :address
 
-  #   # TODO Error handling on attributes_hash variable
-
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
   end
 
-  # self.all -
     # Returns collections of Customer instances
       # Representing all Customers in CSV file
-
   def self.all
     customer_array_of_hashes = CSV.open("data/customers.csv", headers: true).map do |item|
       item.to_h
     end
 
-    # Create array of customers from array of customer hashes
+    # Creating array of Cusomter instances from array of customer hashes
       # EACH hash = Create a new instance CUSTOMER class
     customer_array = customer_array_of_hashes.map do |customer|
      # Using CSV hashes for Customer values
@@ -45,20 +34,10 @@ class Customer
     return customer_array
   end
 
-  # self.find(id) - Returns an instance of Customer where values
+  # RETURNING an instance of Customer where values
     # in id field in the CSV match the passed parameter
   def self.find(id)
-    # Returns customer if id is found
+    # Returns Customer if id is found
     return self.all.find { |customer| customer.id == id }
   end
-
 end
-#
-cassy = Customer.new(5, "cassya@gmail.com", {
-  street: "123 sunny lane",
-  city: "Palmer",
-  state: "AK",
-  zip: "99645"
-  })
-
-# binding.pry
--- lib/order.rb
+++ lib/order.rb
@@ -2,15 +2,11 @@ require 'pry'
 require 'awesome_print'
 require_relative '../lib/customer'
 
-# Create a class called `Order`.
 class Order
+  # Setting attributes to be read-only
   attr_reader :id, :products, :customer, :fulfillment_status
 
-  # When I changed products parameter to products = {} and @ products
-  # to @products = products I get an error
-  # Think it's linked to customer somehow, when I add customer,
-  # The error occurs/works without customer
-  # When I moved customer to the end, it's fine...why?
+  # Initializing with fultillment_status defaulting to :pending
   def initialize(id, products, customer, fulfillment_status=:pending)
     @id = id
     @products = products
@@ -25,76 +21,66 @@ class Order
     end
   end
 
-    def total
-      # Return 0 if empty
-      if products.length == 0
-        return 0
-      else
-        # Summing products
-        sum = products.values.reduce(:+)
-        # Calculating tax
-        tax = sum * 0.075
-        # Returning total
-        return (sum + tax).round(2)
+  # Calculating total from products
+  def total
+    # RETURN 0 IF products is empty
+    if products.length == 0
+      return 0
+    else
+      # SUMMING products
+      sum = products.values.reduce(:+)
+      # CALCULATING tax
+      tax = sum * 0.075
+      # RETURNING total
+      return (sum + tax).round(2)
+    end
+  end
+
+  def add_product(product_name, price)
+    # ADDING data to hash
+    # IF already in hash, RAISE ArgumentError
+    if @products.include?(product_name)
+      raise ArgumentError, 'Product has already been added'
+    else
+      @products[product_name] = price
+    end
+  end
+
+  # RETURNS a collection of Order instances,
+  # representing all of the Orders described in the CSV file
+  def self.all
+   # OPENING orders.csv
+    data = CSV.open("data/orders.csv", headers: true).map do |item|
+      # CREATING a new order instance for each item
+      # id - Set to be an integer value
+      id = item["id"].to_i
+
+      # customer - instance of customer
+      customer = Customer.find(item["customer"].to_i)
+
+      # status - fulfillment status as symbol
+      status = item["status"].to_sym
+
+      # Creating empty hash to hold product key/value pairs
+      product_hash = {}
+      # SPLITTING products into pairs by ;
+      item["products"].split(";").map do |pair|
+        # SPLITTING each product key/value pair by :
+        item = pair.split(":")
+        # ADDING item to product_hash
+        product_hash[item[0]] = item[1].to_f
       end
+      # SETTING old products value to new product hash
+      products = product_hash
+      # CREATING instance of Order
+      Order.new(id, products, customer, status)
     end
+  end
 
-    def add_product(product_name, price)
-      # Adds data to hash
-      # If already in hash, raise ArgumentError
-      if @products.include?(product_name)
-        raise ArgumentError, 'Product has already been added'
-      else
-        @products[product_name] = price
-      end
-    end
-
-    # returns a collection of Order instances,
-    # representing all of the Orders described in the CSV file
-
-    def self.all
-     # Opening orders.csv
-      data = CSV.open("data/orders.csv", headers: true).map do |item|
-        # Create a new order instance for each item
-        # id
-        id = item["id"].to_i
-
-        # customer - return instance of customer
-        customer = Customer.find(item["customer"].to_i)
-        # fulfillment status
-        status = item["status"].to_sym
-
-        # Creating empty hash to hold product key/value pairs
-        product_hash = {}
-        # Splitting products by ;
-        item["products"].split(";").map do |pair|
-            # Splitting each product by :
-            item = pair.split(":")
-            # Adding item as key and dollar value as value
-            product_hash[item[0]] = item[1].to_f
-        end
-          products = product_hash
-          Order.new(id, products, customer, status)
-      end
-    end
-
-    # self.find(id) - Returns an instance of Order where values
-      # in id field in the CSV match the passed parameter
-    def self.find(id)
-      # Returns customer if id is found
-      return self.all.find { |order| order.id == id }
-    end
+  # self.find(id) - RETURNS an instance of Order where values
+    # in id field in the CSV match the passed parameter
+  def self.find(id)
+    # RETURNS order if id is found
+    return self.all.find { |order| order.id == id }
+  end
 end
-
-
-address = {
-  street: "123 Main",
-  city: "Seattle",
-  state: "WA",
-  zip: "98101"
-}
-cassy = Customer.new(123, "a@a.co", address)
-pending_order = Order.new(123, {}, cassy)
-paid_order = Order.new(123, {"banana" => 1.99, "cracker" => 3.00}, cassy, :paid)
-
-binding.pry
