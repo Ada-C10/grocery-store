7d2f9cda3f6e4b08940ed4017f638b830c17c72d
Amanda Ungco <amanda.ungco@gmail.com>
2018-08-26 14:36:45 -0700

    added parse_product_array method to convert csv data

--- lib/order.rb
+++ lib/order.rb
@@ -1,24 +1,6 @@
 require_relative 'csv'
 
-def self.load_data(filename) #take file name and returns data from file in array of hashes
-  data = CSV.open(filename,'r', headers:false).map do |line|
-  line.to_a
-  end
-  return data
-end
 
-def self.format_data(data)
-    data.each do |individual|
-      id = individual[0].to_i
-      products = individual[1]
-      customer =
-      customer_status
-
-    @@orders << self.new(id, products, customer, fulfillment_status = :pending)
-
-    end
-  return @@orders
-end
 
 class Order
   def initialize(id, products, customer, fulfillment_status = :pending)
@@ -32,6 +14,38 @@ class Order
 
   attr_reader :id
   attr_accessor :products, :products, :customer, :fulfillment_status
+
+  def self.load_data(filename) #take file name and returns data from file in array of hashes
+    data = CSV.open(filename,'r', headers:false).map do |line|
+    line.to_a
+    end
+    return data
+  end
+
+  #Lobster:17.18;Annatto seed:58.38;Camomile:83.21
+
+  def parse_product_array(string)#try regex /:|;/
+    products_hash = {}
+    products_array = string.split(';') #[lobster:17, annato seed:58]
+    products_array.each do |product|
+      single_item_array = product.split(':')
+      products_hash[single_item_array[0]]= single_item_array[1].to_f
+    end
+    return products_hash #hash {item:price}
+  end
+
+  def self.format_data(data)
+      data.each do |individual|
+        id = individual[0].to_i
+        products = parse_product_array(individual[1])
+        customer = individual[2]
+        fulfillment_status = individual[3]
+
+      @@orders << self.new(id, products, customer, fulfillment_status = :pending)
+
+      end
+    return @@orders
+  end
 
   def total
     order_total = @products.values.reduce(0.0){ |total, price| total + price}
@@ -58,9 +72,7 @@ class Order
   end
 end
 
-
-end
-
+# parse_product_array('Lobster:17.18;Annatto seed:58.38;Camomile:83.21')
 # products = { "banana" => 1.99, "cracker" => 3.00 }
 # friday = Order.new(1337, products, 'customer')
 # p order.total(products)
