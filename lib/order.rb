98b31b1e3b5f21f9a062bdc710938dbe0371b141
Valerie Gidzinski <valgidzi@gmail.com>
2018-08-25 19:45:17 -0700

    Refactor Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,55 +1,49 @@
 class Order
+
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
 
+
   VALID_STATUSES = [:pending, :paid, :processing, :shipped, :complete]
 
+
+  # Instance Methods
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
 
-
     unless VALID_STATUSES.include? (fulfillment_status)
       raise ArgumentError
     end
   end
 
+
   def total
-    all_prices = @products.values
-    subtotal = all_prices.sum
-    tax = subtotal * 0.075
-    total = subtotal + tax
+    subtotal = @products.values.sum
+    tax = 0.075
+
+    total = subtotal + (subtotal * tax)
+
     return total.round(2)
   end
 
+
   def add_product(product, price)
-    products = @products.keys
-    if products.include? (product)
-      raise ArgumentError
-    else
-      @products[product] = price
-    end
+    @products.keys.include?(product) ? raise(ArgumentError) : @products[product] = price
   end
 
+
+  # Class Methods
   def self.all
     orders = []
 
     CSV.read('data/orders.csv').each do |order|
 
       id = order[0].to_i
-
-      products_hash = {}
-      order_products = order[1].split(';')
-      order_products.each do |product|
-        separate = product.split(':')
-        products_hash[separate[0]] = separate[1].to_f
-      end
-      products = products_hash
-
+      products = FormatData.parse_to_hash(order[1])
       customer = Customer.find(order[2].to_i)
-
       fulfillment_status = order[3].to_sym
 
       orders << Order.new(id, products, customer, fulfillment_status)
@@ -58,11 +52,28 @@ class Order
     return orders
   end
 
+
   def self.find(id)
     orders = Order.all
+
     orders.find do |order|
       order.id == id
     end
   end
 
+end
+
+
+# Helper Methods
+module FormatData
+  def self.parse_to_hash(string)
+    hash = {}
+
+    string.split(';').each do |item|
+      pair = item.split(':')
+      hash[pair[0]] = pair[1].to_f
+    end
+
+    return hash
+  end
 end
