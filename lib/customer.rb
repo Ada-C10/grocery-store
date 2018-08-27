41fcf2978c2ba2c4933d257d98ae0d8b7b6f472c
KatherineJF <kjfitzpat@hotmail.com>
2018-08-26 23:13:10 -0700

    Wave 1 done tests passed wave 2 in progress

---
+++ lib/customer.rb
@@ -0,0 +1,43 @@
+#class Cusotmer
+require 'csv'
+require 'awesome_print'
+require 'pry'
+
+class Customer
+
+  #@@id = 123
+
+  attr_reader :id
+@@customers = []
+  attr_accessor :email, :address
+
+  def initialize(id, email, address = {street: "", city:"", state: "", zip:""})
+    @id = id
+    @email = email
+    @address = address
+
+
+  end
+
+
+
+  def self.all(filename)
+    CSV.read(filename).map do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+      address = {street: customer[2], city: customer[3], state: customer[4], zip: customer[5]}
+      @@customers<<customer.self(id,email, address)
+    end
+    return @@customers
+  end
+
+
+
+end
+
+
+
+
+
+
+end
