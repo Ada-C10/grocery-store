bdeb88ae034c0167f3c36791f240482ec1319552
Kay <kayxn23@gmail.com>
2018-08-27 08:18:52 -0700

    refactored to change valid_status to a class variable, and deleted some irrelevant notes

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,5 @@
-# require_relative '../lib/orders'
 require 'pry'
 require 'csv'
-# data/customers.csv'
-#if user dosnt have ID...find(ifnone = nil) { |obj| block } → obj or nil
-# ruby lib/customer.rb  <--put this in terminal to practice run this
-#be in grocery store to run rake, make sure all other things are commented out
 
 class Customer
 
@@ -18,16 +13,14 @@ class Customer
     @address = {:street => address[:street], :city => address[:city], :state => address[:state], :zip => address[:zip]}
   end
 
-  # self.all - returns a collection of Customer instances,
-  # representing all of the Customers described in the CSV file
+  # returns a collection of Customer instances,representing all of the Customers described in the CSV file
   def self.all
     CSV.open('data/customers.csv', 'r').map do |row|
       Customer.new(row[0].to_i, row[1], address = {street: row[2], city: row[3], state: row[4], zip: row[5]})
     end
   end
 
-  # self.find(id) - returns an instance of Customer where
-  # the value of the id field in the CSV matches the passed parameter
+  # returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
   def self.find(id)
     self.all.find do |customer|
       customer.id == id
--- lib/order.rb
+++ lib/order.rb
@@ -3,33 +3,30 @@ require 'csv' #requiring csv ruby module into order.rb
 require 'pry'
 
 class Order
+  @@valid_status = [:pending, :paid, :processing, :shipped, :complete]
+
   attr_reader :id, :products
   attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, products, customer, fulfillment_status= :pending) #should id and ful_st be different?
+  def initialize(id, products, customer, fulfillment_status= :pending)
     @id = id
     @products = products
-    @customer = customer#(should this be different?)
-    valid_status = [:pending, :paid, :processing, :shipped, :complete]
-    raise ArgumentError.new("That is an invalid fulfillment status") unless valid_status.include?(fulfillment_status)
+    @customer = customer
+    raise ArgumentError.new("That is an invalid fulfillment status") unless @@valid_status.include?(fulfillment_status)
     @fulfillment_status = fulfillment_status
   end
 
-#sum up products to calc total
-#add 7.5% tax
-#round to 2 decimal places
+  #sums up products to calc total and adds 7.5% tax
   def total
     @total = 0
     @products.each_value do |cost|
       @total += cost
     end
+    #0.075 represents the sales tax 
     @total += @total *= 0.075
     return @total.round(2)
   end
-# An add_product method which will take in two parameters,
-# product name and price, and add the data to the product collection
-# If a product with the same name has already been
-# added to the order, an ArgumentError should be raised
+
   def add_product(name, price)
     raise ArgumentError.new("woah! this product is already on the order") if products.has_key?(name)
     @products[name] = price
@@ -44,16 +41,14 @@ class Order
     return products_hash = Hash[array2.map {|k,v| [k,v.to_f]}]
   end
 
-#   self.all - returns a collection of Order instances,
-#   representing all of the Orders described in the CSV file
+# returns a collection of Order instances, representing all of the Orders described in the CSV file
   def self.all
     CSV.open('data/orders.csv', 'r').map do |row|
       Order.new(row[0].to_i, self.convert_to_hash(row[1]), Customer.find(row[2].to_i), row[3].to_sym) # row[2] issue Expected "25" to be a kind of Customer, not String.
     end
   end
 
-# self.find(id) - returns an instance of Order where
-# the value of the id field in the CSV matches the passed parameter
+# returns an instance of Order where the value of the id field in the CSV matches the passed parameter
   def self.find(id)
     self.all.find do |order|
       order.id == id
