329a905d9f1c5afddef02f70c70c4636d08ce19f
Melissa O'Hearn <melissa.ohearn@gmailcom>
2018-08-26 19:39:50 -0700

    made self.all and self.find methods for order.rb, finished the order_spec.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,8 +6,7 @@ class Customer
 # helper mothods: automatically add reader and writer methods for these varibales. attr_accessor allows for write and read access
 attr_reader :id
 attr_accessor :email, :address
-@@all
-@@find
+
 
 # constructor: this will automatically called wen Customer.new is invoked
   def initialize(id, email, address)
@@ -18,17 +17,21 @@ attr_accessor :email, :address
   end
   # class methods
   def self.all
-    # inport csv file, into customers_arry. map over that array to create a new instance of Customer. Return the instances of all customers
-    customers_array = CSV.read("data/customers.csv")
-    all_customers = customers_array.map do |customer|
-      Customer.new(customer[0].to_i, customer[1], customer[2])
-    end
-    return all_customers
-
+    # inport csv file, into customers_arry. map over that array to create a new instance of Customer. Return the instances of all customers. turn the address into one element
+    customers_data = CSV.read("data/customers.csv")
+      customer_all = customers_data.map do |customer|
+        address = "#{customer[2]} #{customer[3]}, #{customer[4]}"
+        Customer.new(customer[0].to_i, customer[1], address)
+      end
+    return customer_all
   end
 
   def self.find(id)
-    return @@find
+    customers = Customer.all
+    find_customer = customers.select do |customer|
+      customer.id == id
+    end
+    return find_customer.first
   end
 
 
