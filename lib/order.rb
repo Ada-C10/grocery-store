9cb8e8e20aedae2f8c21cbef83512338da79bb9f
Layla <layla.bristol@gmail.com>
2018-08-26 22:27:52 -0700

    wave 2 complete

--- lib/order.rb
+++ lib/order.rb
@@ -2,6 +2,7 @@ class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
 
+  DEFAULT_CSV_FILE = "data/orders.csv"
   VALID_STATUSES = [:pending, :paid, :processing, :shipped, :complete]
 
   def initialize(id, products, customer, fulfillment_status = :pending)
@@ -32,5 +33,41 @@ class Order
 
   end
 
+  def self.all(csv_file = DEFAULT_CSV_FILE)
+    all_orders = []
+
+    CSV.open(csv_file, "r").each do |order|
+      all_orders << self.csv_to_order(order)
+    end
+
+    return all_orders #array of order objects
+  end
+
+  def self.find(id, csv_file = DEFAULT_CSV_FILE)
+    CSV.open(csv_file, "r").each do |order|
+      if order[0].to_i == id
+        return self.csv_to_order(order) # returns order object
+      end
+    end
+  end
+
+  def self.csv_to_order(csv_row)
+    order_id = csv_row[0].to_i
+    order_products = self.parse_products(csv_row[1])
+    order_customer = Customer.find(csv_row[2].to_i)
+    order_status = csv_row[3].to_sym
+    return self.new(order_id, order_products, order_customer, order_status)
+  end
+
+  def self.parse_products(products)
+    parsed_products = {}
+
+    products.split(";").each do |single_product|
+      (product_name, product_cost) = single_product.split(":")
+      parsed_products[product_name] = product_cost.to_f
+    end
+
+    return parsed_products
+  end
 
 end
